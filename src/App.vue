e44763c909cdbd31cfc90be0c34007e8b100d635
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2023-12-04 15:26:58 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+
+
+
+</script>
+
+<template>
+<AppHeader />
+
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css";
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+import TopHeader from './TopHeader.vue';
+
+export default {
+    data() {
+        return {};
+    },
+    components: { TopHeader }
+}
+</script>
+
+<template>
+<TopHeader />
+</template>
+
+<style lang="scss" scoped>
+@use "../style/partials/mixin" as *;
+@use "../style/partials/variables" as *;
+
+</style>
